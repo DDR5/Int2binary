19f03e8696fdf63ced0aab7cd6aad0c81a3e6722
Kingofpython <didyouhidethebody@gmail.com>
2014-01-05 13:57:35 -0500

    Replaced a useless line
    I updated the loop to run more seamlessly and speed-o-lishious

--- Source/sourceCode.py
+++ Source/sourceCode.py
@@ -1,16 +1,14 @@
 #This program will convert interger values to their binary counter part
 import time, sys
 con = True
-while con == True:
+while con:
     x = input('Input a interger value to convert to binary. ')
     print('Now converting \n')
     print (bin(x) + ' - Ignore the 0b infront of the binary values, its not important for what you want')
     time.sleep(3)
     y = raw_input('Would you like to input another number? [Y/N]').lower()
-    if y == "y":
-     con = con   
-    else:
-        con = False
+    if y == "n":
+     con = False   
 
 
     
