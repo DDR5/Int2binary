ee6160d9e00fa235ac7c71bbdd14e46ea3d89e55
Kingofpython <didyouhidethebody@gmail.com>
2014-01-05 13:27:35 -0500

    Took out an un-needed line
    The extra print just added un-needed bytes. I removed it and replaced
    with a new line

--- Source/sourceCode.py
+++ Source/sourceCode.py
@@ -3,8 +3,7 @@ import time, sys
 con = True
 while con == True:
     x = input('Input a interger value to convert to binary. ')
-    print('Now converting')
-    print('')
+    print('Now converting \n')
     print (bin(x) + ' - Ignore the 0b infront of the binary values, its not important for what you want')
     time.sleep(3)
     y = raw_input('Would you like to input another number? [Y/N]').lower()
